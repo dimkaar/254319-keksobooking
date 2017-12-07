803ad7104d3b1e6fd416503b4563798279dfdedf
dimkaar <dimkaar1102@gmail.com>
2017-12-07 15:21:15 +0300

    An adsArray has added to the util file

--- js/util.js
+++ js/util.js
@@ -5,18 +5,21 @@
   var ESC_KEYCODE = 27;
 
   window.util = {
-    isEscEvent: function (evt, action) {
+    isEscEvent: function (evt, action, nextAction) {
       if (evt.keyCode === ESC_KEYCODE) {
         action();
+        nextAction();
       }
     },
-    isEnterEvent: function (evt, action) {
+    isEnterEvent: function (evt, action, nextAction) {
       if (evt.keyCode === ENTER_KEYCODE) {
         action();
+        nextAction();
       }
     },
     getRandomArbitrary: function (min, max) {
       return Math.floor(Math.random() * (max - min) + min);
-    }
+    },
+    adsArray: []
   };
 })();
