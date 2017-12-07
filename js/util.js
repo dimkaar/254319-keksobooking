03d8e02997acaec378372839ec56c9681f4400f4
dimkaar <dimkaar1102@gmail.com>
2017-12-07 14:08:30 +0300

    Util module has been added to the file

---
+++ js/util.js
@@ -0,0 +1,22 @@
+'use strict';
+
+(function () {
+  var ENTER_KEYCODE = 13;
+  var ESC_KEYCODE = 27;
+
+  window.util = {
+    isEscEvent: function (evt, action) {
+      if (evt.keyCode === ESC_KEYCODE) {
+        action();
+      }
+    },
+    isEnterEvent: function (evt, action) {
+      if (evt.keyCode === ENTER_KEYCODE) {
+        action();
+      }
+    },
+    getRandomArbitrary: function (min, max) {
+      return Math.floor(Math.random() * (max - min) + min);
+    }
+  };
+})();
