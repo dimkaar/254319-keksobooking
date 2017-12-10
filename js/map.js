64047ef3ebff954b5f5d70802117b647fe3ef3bb
dimkaar <dimkaar1102@gmail.com>
2017-12-10 21:59:39 +0300

    The drag and drop has been added to main pin as it should be as in task

--- js/map.js
+++ js/map.js
@@ -2,6 +2,16 @@
 
 window.mapModule = (function () {
   var mapModule = {};
+  mapModule.location = '';
+  var MAIN_PIN_HEIGHT = 65;
+  var MAIN_PIN_SPADE = 16;
+  var pinsMap = window.util.mapBlock.querySelector('.map__pins');
+  var pinsMapStyle = getComputedStyle(pinsMap);
+  var minY = 100;
+  var maxY = 500;
+  var minX = pinsMap.offsetTop;
+  var maxX = pinsMapStyle.width;
+  maxX = parseInt(maxX.replace('px', ''), 10);
   var init = function () {
     var noticeFormFieldsets = window.util.noticeForm.querySelectorAll('fieldset');
     window.util.mapBlock.classList.add('map--faded');
@@ -12,14 +22,56 @@ window.mapModule = (function () {
   };
 
   mapModule.activateMap = function () {
-    var pinsMap = window.util.mapBlock.querySelector('.map__pins');
     window.util.mapBlock.classList.remove('map--faded');
     pinsMap.appendChild(window.util.fragment);
   };
 
-  mapModule.escKeydownHandler = function (evt) {
+  mapModule.escKeyDownHandler = function (evt) {
     window.util.isEscEvent(evt, window.cardModule.removePopup, window.pinModule.removeActivePin);
   };
+
+  mapModule.mainPinMouseDownHandler = function (evt) {
+    var startDragCoords = {
+      x: evt.clientX,
+      y: (evt.clientY - MAIN_PIN_HEIGHT / 2 - MAIN_PIN_SPADE)
+    };
+
+    var mainPinMouseMoveHandler = function (moveEvt) {
+      var shift = {
+        x: startDragCoords.x - moveEvt.clientX,
+        y: startDragCoords.y - moveEvt.clientY
+      };
+
+      startDragCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      if ((window.util.mainPin.offsetTop - shift.y) >= minY && (window.util.mainPin.offsetTop - shift.y) <= maxY) {
+        mapModule.locationY = (window.util.mainPin.offsetTop - shift.y);
+        window.util.mainPin.style.top = mapModule.locationY + 'px';
+      }
+      if (window.util.mainPin.offsetLeft - shift.x >= minX && window.util.mainPin.offsetLeft - shift.x <= maxX) {
+        mapModule.locationX = (window.util.mainPin.offsetLeft - shift.x);
+        window.util.mainPin.style.left = mapModule.locationX + 'px';
+      }
+      mapModule.location = 'x: ' + mapModule.locationX + ', y: ' + mapModule.locationY;
+    };
+
+    var mainPinMouseUpHandler = function () {
+      window.mapModule.activateMap();
+      window.activateForm();
+
+      document.removeEventListener('mousemove', mainPinMouseMoveHandler);
+      document.removeEventListener('mouseup', mainPinMouseUpHandler);
+    };
+
+    document.addEventListener('mousemove', mainPinMouseMoveHandler);
+    document.addEventListener('mouseup', mainPinMouseUpHandler);
+  };
+
+  window.util.mainPin.addEventListener('mouseup', mapModule.mainPinMouseUpHandler);
+  window.util.mainPin.addEventListener('mousedown', mapModule.mainPinMouseDownHandler);
 
   window.onload = init;
   return mapModule;
