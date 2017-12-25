4eb98a01821eca6a8aac3da5ca6243726fffdb9d
dimkaar <dimkaar1102@gmail.com>
2017-12-25 20:19:18 +0300

    Comments have been deleted from the code

--- js/show-card.js
+++ js/show-card.js
@@ -3,21 +3,18 @@
 (function () {
   var article = document.querySelector('template').content.querySelector('article.map__card');
 
-  // onClose добавляем функцию, которая будет вызвана при закрытии.
-  // onClose должна заменять вызовы window.pin.*
   var showCard = function (data, onClose) {
     var removePopup = function () {
-      // document.removeEventListener('keydown', escKeyDownHandler); // <-- может уже не существовать, чиним переносом
       var popup = document.querySelector('.popup');
       if (popup) {
-        document.removeEventListener('keydown', escKeyDownHandler); // починили,
-        popup.remove(); // посмотри потом, функция remove реализована не во всех браузерах
+        document.removeEventListener('keydown', escKeyDownHandler);
+        popup.remove();
       }
     };
 
     var popupCloseClickHandler = function () {
       removePopup();
-      onClose(); // onClose
+      onClose();
     };
 
     var popupCloseKeyDownHandler = function (evt) {
@@ -28,10 +25,8 @@
       window.util.isEscEvent(evt, removePopup, onClose);
     };
 
-    removePopup(); // <-- здесь удаляем popup
+    removePopup();
     var insertBeforeBlock = document.querySelector('.map__filter-container');
-    // if (!window.util.mapBlock.querySelector('.popup')) { // бесполезная проверка, так как у нас выше мы удаляем блок
-    // }
 
     var card = renderAd(data);
     var popupClose = card.querySelector('.popup__close');
@@ -43,16 +38,15 @@
     document.addEventListener('keydown', escKeyDownHandler);
   };
 
-
-  var renderAd = function (adData) {
+  var renderAd = function (ad) {
     var instanceOfAd = article.cloneNode(true);
     var fragment = document.createDocumentFragment();
     var featuresElement = instanceOfAd.querySelector('.popup__features');
     var houseTypeHeader = instanceOfAd.querySelector('h4');
-    instanceOfAd.querySelector('h3').textContent = adData.offer.title;
-    instanceOfAd.querySelector('small').textContent = adData.offer.address;
-    instanceOfAd.querySelectorAll('p')[1].innerHTML = adData.offer.price + '&#x20bd;/ночь';
-    switch (adData.offer.type) {
+    instanceOfAd.querySelector('h3').textContent = ad.offer.title;
+    instanceOfAd.querySelector('small').textContent = ad.offer.address;
+    instanceOfAd.querySelectorAll('p')[1].innerHTML = ad.offer.price + '&#x20bd;/ночь';
+    switch (ad.offer.type) {
       case 'flat': houseTypeHeader.textContent = 'Квартира';
         break;
       case 'house': houseTypeHeader.textContent = 'Дом';
@@ -62,23 +56,21 @@
       default: houseTypeHeader.textContent = 'Не указано';
         break;
     }
-    instanceOfAd.querySelectorAll('p')[2].innerHTML = adData.offer.rooms + ' для ' + adData.offer.guests + ' гостей';
-    instanceOfAd.querySelectorAll('p')[3].textContent = 'Заезд после: ' + adData.offer.checkin + ', выезд до ' + adData.offer.checkout;
+    instanceOfAd.querySelectorAll('p')[2].innerHTML = ad.offer.rooms + ' для ' + ad.offer.guests + ' гостей';
+    instanceOfAd.querySelectorAll('p')[3].textContent = 'Заезд после: ' + ad.offer.checkin + ', выезд до ' + ad.offer.checkout;
     window.util.removeChilds(featuresElement);
-    for (var i = 0; i < adData.offer.features.length; i++) {
+    for (var i = 0; i < ad.offer.features.length; i++) {
       var newLi = document.createElement('li');
-      newLi.classList = 'feature feature--' + adData.offer.features[i];
+      newLi.classList = 'feature feature--' + ad.offer.features[i];
       fragment.appendChild(newLi);
     }
-    instanceOfAd.querySelectorAll('p')[4].textContent = adData.offer.description;
+    instanceOfAd.querySelectorAll('p')[4].textContent = ad.offer.description;
     featuresElement.appendChild(fragment);
-    instanceOfAd.querySelector('.popup__avatar').src = adData.author.avatar;
-
+    instanceOfAd.querySelector('.popup__avatar').src = ad.author.avatar;
     return instanceOfAd;
   };
 
   window.cardModule = {
-    // removePopup: removePopup, // Это нам здесь не надо, мы удаляем эту связь
     showCard: showCard
   };
 })();
